8de300baa0ca3c1ce346bd0caf2188347b4f499a
Jeff Peck <jeffrey.peck@icloud.com>
2026-09-04 00:28:09 -0400

    ghpages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://jeffp123.github.io',
+	base: '/israel2026',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,18 +2,6 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
-});
-
 const entries = defineCollection({
 	loader: glob({ pattern: '**/*.mdx', base: './src/content/entries' }),
 	schema: ({ image }) =>
@@ -26,4 +14,4 @@ const entries = defineCollection({
 		}),
 });
 
-export const collections = { blog, entries };
+export const collections = { entries };
